e5199a1e97ab6f7b25080c469e79b388c4fbf327
trdquang <trrdquang@gmail.com>
2024-10-07 16:16:24 +0700

    Revert "up"
    This reverts commit c33cbe4e315d5327eebbb15322c3194b7a16e381.

--- Practice1_BookStore/src/Main.java
+++ Practice1_BookStore/src/Main.java
@@ -1,15 +1,9 @@
-import dto.OrderDTO;
 import dto.UserDTO;
-import entity.OrderItem;
-import model.OrderItemModel;
 import model.UserModel;
-import service.impl.OrderService;
 import service.impl.UserService;
 import util.FileUtil;
 
 import java.io.*;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.List;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -18,15 +12,10 @@ public class Main {
 
 
     public static void main(String[] args) throws IOException{
-        OrderService orderService = new OrderService();
-        List<OrderDTO> or = orderService.getAll();
-//        for(OrderDTO it: or){
-//            System.out.println(it.getId() + "; " + it.getBookId() + "; " + it.getSell() + "; " + it.getUserId() + "; " + it.getDate());
-//        }
-//
-//        orderService.save(new OrderItemModel("1", "2", 3, new Date()));
-
-        orderService.deleteById("1");
+        UserService userService = new UserService();
+//        userService.save(new UserModel(  "abc", "2", "3"));
+        UserDTO u = userService.findById("user11");
+        userService.edit(u);
     }
 
 }
--- Practice1_BookStore/src/service/IOrrderService.java
+++
@@ -1,9 +0,0 @@
-package service;
-
-import dto.OrderDTO;
-import mapper.OrderItemMapper;
-import model.OrderItemModel;
-
-public interface IOrrderService extends BaseService<OrderDTO, OrderItemModel>{
-    void save(OrderItemModel orderItemModel);
-}
