2b4b8aa08759e1d10e9a4a90728543e2f651daab
trdquang <trrdquang@gmail.com>
2024-10-07 19:39:01 +0700

    Remove all files from dev branch

--- Practice1_BookStore/src/util/FilePath.java
+++
@@ -1,9 +0,0 @@
-package util;
-
-public class FilePath {
-    public static final String AUTHOR_FILE_PATH = "src/util/author.txt";
-    public static final String BOOK_FILE_PATH = "src/util/book.txt";
-    public static final String CART_FILE_PATH = "src/util/cart.txt";
-    public static final String ROLE_FILE_PATH = "src/util/role.txt";
-    public static final String USER_FILE_PATH = "src/util/user.txt";
-}
