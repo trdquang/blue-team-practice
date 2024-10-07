9e4edb0976776061d3b15808ee6040048581d8f0
unknown <duong26122003dddppp@gmail.com>
2024-10-07 16:49:37 +0700

    fix bug book service crud

--- Practice1_BookStore/src/service/impl/BookServiceImplement.java
+++ Practice1_BookStore/src/service/impl/BookServiceImplement.java
@@ -24,7 +24,7 @@ public class BookServiceImplement implements IBookService {
     public List<BookDTO> getAll() throws IOException {
         if (fileUtil.readFileBuffer(bookFilePath).size() != 0 && fileUtil.readFileBuffer(bookFilePath).get(0) != null) {
             List<Book> bookList = fileUtil.readFileBuffer(bookFilePath).stream().map(x -> convertToEntity(x)).toList();
-            return bookList.stream().map(x -> bookMapper.toDTO(x)).toList();
+            return bookList.stream().map(x -> bookMapper.toDTO(x)).collect(Collectors.toList());
         } else {
             return new ArrayList<BookDTO>();
         }
@@ -34,19 +34,27 @@ public class BookServiceImplement implements IBookService {
     public void save(BookModel model) throws IOException {
         Book book = bookMapper.modelToEntity(model);
         int id = 0;
-        if (fileUtil.readFileBuffer(bookFilePath).get(0) != null) {
+        if (fileUtil.readFileBuffer(bookFilePath).size()!=0 && fileUtil.readFileBuffer(bookFilePath).get(0)!=null) {
             id = fileUtil.readFileBuffer(bookFilePath).size() + 1;
+            book.setId(String.valueOf(id));
+            book.setAuthorIds(model.getAuthorIds());
+            StringBuilder content = new StringBuilder(book.getId() + "," + book.getName() + "," + book.getQuantity());
+            for (int i = 0; i < book.getAuthorIds().size(); i++) {
+                content.append("," + book.getAuthorIds().stream().toList().get(i));
+            }
+            content.append("\n");
+            fileUtil.writeFile("D:\\java_project\\blue-team-practice\\Practice1_BookStore\\src\\util\\book.txt", content.toString(), true);
         } else {
             id = 1;
+            book.setId(String.valueOf(id));
+            book.setAuthorIds(model.getAuthorIds());
+            StringBuilder content = new StringBuilder(book.getId() + "," + book.getName() + "," + book.getQuantity());
+            for (int i = 0; i < book.getAuthorIds().size(); i++) {
+                content.append("," + book.getAuthorIds().stream().toList().get(i));
+            }
+            content.append("\n");
+            fileUtil.writeFile("D:\\java_project\\blue-team-practice\\Practice1_BookStore\\src\\util\\book.txt", content.toString(), false);
         }
-        book.setId(String.valueOf(id));
-        book.setAuthorIds(model.getAuthorIds());
-        StringBuilder content = new StringBuilder(book.getId() + "," + book.getName() + "," + book.getQuantity());
-        for (int i = 0; i < book.getAuthorIds().size(); i++) {
-            content.append("," + book.getAuthorIds().stream().toList().get(i));
-        }
-        content.append("\n");
-        fileUtil.writeFile("D:\\java_project\\blue-team-practice\\Practice1_BookStore\\src\\util\\book.txt", content.toString(), true);
     }
 
     @Override
@@ -56,33 +64,35 @@ public class BookServiceImplement implements IBookService {
         sltBook.setName(bookDTO.getName());
         sltBook.setQuantity(bookDTO.getQuantity());
         sltBook.setAuthorIds(bookDTO.getAuthorIds());
-        for (BookDTO dto:bookDTOList){
-            StringBuilder content=new StringBuilder(dto.getId()+","+dto.getName()+","+dto.getQuantity());
-            for (int authorId: dto.getAuthorIds()){
-                content.append(","+authorId);
+        fileUtil.writeFile(bookFilePath, "", false);
+        for (BookDTO dto : bookDTOList) {
+            StringBuilder content = new StringBuilder(dto.getId() + "," + dto.getName() + "," + dto.getQuantity());
+            for (int authorId : dto.getAuthorIds()) {
+                content.append("," + authorId);
             }
             content.append("\n");
-            fileUtil.writeFile(bookFilePath,content.toString(),false);
+            fileUtil.writeFile(bookFilePath, content.toString(), true);
         }
     }
 
     @Override
     public void deleteById(String id) throws IOException {
         List<BookDTO> bookList = getAll();
-        bookList.remove(bookList.stream().filter(x->x.getId().equals(id)).toList().get(0));
-        for (BookDTO dto:bookList){
-            StringBuilder content=new StringBuilder(dto.getId()+","+dto.getName()+","+dto.getQuantity());
-            for (int authorId: dto.getAuthorIds()){
-                content.append(","+authorId);
+        bookList.remove(bookList.stream().filter(x -> x.getId().equals(id)).toList().get(0));
+        fileUtil.writeFile(bookFilePath, "", false);
+        for (BookDTO dto : bookList) {
+            StringBuilder content = new StringBuilder(dto.getId() + "," + dto.getName() + "," + dto.getQuantity());
+            for (int authorId : dto.getAuthorIds()) {
+                content.append("," + authorId);
             }
             content.append("\n");
-            fileUtil.writeFile(bookFilePath,content.toString(),false);
+            fileUtil.writeFile(bookFilePath, content.toString(), true);
         }
     }
 
     @Override
     public BookDTO findById(String id) throws IOException {
-        if (fileUtil.readFileBuffer(bookFilePath).size() != 0 && fileUtil.readFileBuffer(bookFilePath).get(0) != null) {
+        if (fileUtil.readFileBuffer(bookFilePath).size() != 0 && !fileUtil.readFileBuffer(bookFilePath).get(0).equals("")) {
             return bookMapper.toDTO(fileUtil.readFileBuffer(bookFilePath).stream().map(x -> convertToEntity(x)).toList().get(0));
         } else {
             return null;
